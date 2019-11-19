a845d4b7554c424fa25bea14032064aebb09c573
Frederick Johnson <fritzj96@gmail.com>
2019-11-19 14:31:47 -0800

    added createUser method to api

--- data/firebase/api.js
+++ data/firebase/api.js
@@ -1,5 +1,5 @@
-import app from 'firebase/app';
-import auth from 'firebase/auth';
+import app from "firebase/app";
+import "firebase/auth";
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -7,13 +7,16 @@ const config = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-}
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+};
 
 export class Firebase {
-    constructor() {
-        app.initializeApp(config);
-        
-        this.auth = app.auth();
-    }
+  constructor() {
+    app.initializeApp(config);
+    this.auth = app.auth();
+  }
+
+  createUser = (email, password) => {
+    this.auth.createUserWithEmailAndPassword(email, password);
+  }
 }
