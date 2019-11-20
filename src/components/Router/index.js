e9c9a2c7bcc70607ef52a82aca9970bf6f4eeb34
Frederick Johnson <fritzj96@gmail.com>
2019-11-20 11:40:32 -0800

    added router constants to the router in place of strings

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom"
 import { SignUpPage, SignInPage } from "../../pages";
 import { Navbar } from "../Navbar";
+import * as routes from "../../constants/routes";
 
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Navbar />
-            <Route path="/signin" component={SignInPage} />
-            <Route path="/signup" component={SignUpPage} />
+            <Route path={routes.signIn} component={SignInPage} />
+            <Route path={routes.signUp} component={SignUpPage} />
         </BrowserRouter>
     )
 }
