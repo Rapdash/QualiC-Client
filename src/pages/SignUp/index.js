487f0d1759569993a2baf6b73613acb9f21d082b
Frederick Johnson <fritzj96@gmail.com>
2019-11-20 11:40:59 -0800

    removed withRouter HOC

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,10 @@
 import React, { useContext, useState } from "react";
 import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
-import { withRouter } from "react-router-dom";
 
 import * as routes from "../../constants/routes";
 import { FirebaseContext } from '../../data/firebase';
 
-const SignUpPageBase = ({ history }) => {
+export const SignUpPage = ({ history }) => {
   const firebase = useContext(FirebaseContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -32,28 +31,29 @@ const SignUpPageBase = ({ history }) => {
       <Row>
         <Col xs={{ span: 12 }} sm={{ span: 10, offset: 1}} md={{ span: 6, offset: 3 }} lg={{ span: 4, offset: 4 }}>
           <Card>
-            <Card.Header className="bg-primary">
+            <Card.Header className="bg-dark">
               <Card.Title className="text-center text-white my-1">Create Account</Card.Title>
             </Card.Header>
             <Card.Body>
               <Form onSubmit={onSubmit}>
                 <Form.Group controlId="emailForm">
                   <Form.Label>Email:</Form.Label>
-                  <Form.Control size="large" type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter Email" autoComplete="new-email"/>
+                  <Form.Control size="lg" type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter Email" autoComplete="new-email"/>
                 </Form.Group>
                 <Form.Group controlId="passwordForm">
                   <Form.Label>Password:</Form.Label>
-                  <Form.Control size="large" type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Enter Password" autoComplete="new-password"/>
+                  <Form.Control size="lg" type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Enter Password" autoComplete="new-password"/>
                 </Form.Group>
                 <Form.Group controlId="confirmPasswordForm">
                   <Form.Label>Confirm Password:</Form.Label>
-                  <Form.Control size="large" type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder="Enter Password Again" autoComplete="new-password"/>
+                  <Form.Control size="lg" type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder="Enter Password Again" autoComplete="new-password"/>
                 </Form.Group>
-                <Button variant="primary" block size="large" type="submit">
-                  Sign In
+                <Button variant="primary" block size="lg" type="submit">
+                  Sign Up
                 </Button>
                 { error &&  <Form.Text as="h4" className="text-danger mt-4 text-center">{error}</Form.Text> }
               </Form>
+              <Button variant="outline-dark" className="mt-3" block onClick={() => history.push(routes.signIn)}>Sign In Instead</Button>
             </Card.Body>
           </Card>
         </Col>
@@ -61,4 +61,3 @@ const SignUpPageBase = ({ history }) => {
     </Container>
   );
 };
-export const SignUpPage = SignUpPageBase
